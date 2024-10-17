1fe7088c6d1e55e6ba5811620d3297f1d0465920
Daria Ohulchanska <daria.ohulchanska@gmail.com>
2024-10-17 12:42:22 +0200

    Generate data models using DBFirst.

---
+++ src/JobScheduler.Data/DbFirstModels/Job.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JobScheduler.Data.Models;
+
+public partial class Job
+{
+    public Guid Id { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public int Status { get; set; }
+
+    public DateTime? ScheduledAt { get; set; }
+
+    public DateTime? StartedAt { get; set; }
+
+    public DateTime? CompletedAt { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+}
